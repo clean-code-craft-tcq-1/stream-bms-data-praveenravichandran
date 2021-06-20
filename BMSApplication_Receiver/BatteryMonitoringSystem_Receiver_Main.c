eeb544f751fd8e978a1b932506bd7fea31a97d29
karippalrohith <77481086+karippalrohith@users.noreply.github.com>
2021-06-20 20:53:11 +0530

    Update BatteryMonitoringSystem_Receiver_Main.c

--- BMSApplication_Receiver/BatteryMonitoringSystem_Receiver_Main.c
+++ BMSApplication_Receiver/BatteryMonitoringSystem_Receiver_Main.c
@@ -36,12 +36,66 @@
  =======                              METHODS                            =======
  =============================================================================*/
 
-#ifndef UNIT_TEST   	
+#ifndef UNIT_TEST   
+int  readInputFromPipe(char *stringIP)
+{
+	int length=0;
+	if(stringIP != NULL)
+	{
+		scanf("%s",stringIP);
+    		len = strlen(stringIP);
+    
+    		stringIP[length] = '\n';
+    		stringIP[length + 1] = '\0';
+		
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
 int main()
 {
 	char stringIP[25];
 	
+	int readStatus = readInputFromPipe(stringIP);
 	
+	if(readStatus == 1)
+	{
+		float paramBuffer[2] = { 0.f };
+		
+		  int i = 0, j = 0; //index counter
+
+ 		char par[15]; //string for each parameter 
+  		int parNum = 0;  //count of parameters
+
+ 
+ 		 while ((stringIP[i] != '\n'))
+		 {
+    
+    			/* Read each parameter */
+    			for (j = 0;stringIP[i] != ',';j++, i++)
+    			{
+      				par[j] = stringIP[i];
+    			}
+
+    			par[j] = '\0'; //Adding end of string
+    
+    			/* Convert string to float number to evaluate further */
+    			paramBuffer[parNum] = atof(par);
+    
+    			printf("Paramter value = %f\n", paramBuffer[parNum]); //for debug purpose
+
+    			/* Iterate to next parameter in the buffer*/ 
+    			parNum++; 
+
+    			/*Escape Comma separated character*/
+    			i++;
+  		}
+		
+	}
 	return 0;
 }
 #endif
