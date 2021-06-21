0a6c0cd97a336ee3edd614b79fc2233eb0061c02
karippalrohith <77481086+karippalrohith@users.noreply.github.com>
2021-06-21 21:20:56 +0530

    Update BatteryMonitoringSystem_Receiver_Main.h

--- BMSApplication_Receiver/BatteryMonitoringSystem_Receiver_Main.h
+++ BMSApplication_Receiver/BatteryMonitoringSystem_Receiver_Main.h
@@ -1,2 +1,5 @@
 int  readInputFromPipe(char *paramIP);
 float getParameterValue(char * TempString);
+int calculateMinParameterValue(float paramValue, float paramMin);
+int calculateMaxParameterValue(float paramValue, float paramMax);
+void printParameter(char paramName[100],char paramStatus[100],float paramValue);
