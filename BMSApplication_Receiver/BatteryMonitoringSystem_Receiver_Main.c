a0ea6b363a21dafe2ba24606711baad22f55843b
karippalrohith <77481086+karippalrohith@users.noreply.github.com>
2021-06-21 08:09:14 +0530

    Update BatteryMonitoringSystem_Receiver_Main.c

--- BMSApplication_Receiver/BatteryMonitoringSystem_Receiver_Main.c
+++ BMSApplication_Receiver/BatteryMonitoringSystem_Receiver_Main.c
@@ -71,6 +71,12 @@ int main(int argc,char *argv[])
 	
 	FILE *in=fopen(*++argv,"r");
 	char c;
+	          while (fgets(stringIP, sizeof stringIP, stdin);)  
+             { 
+                 		printf("%s\n",stringIP);
+		printf("%c\n",stringIP[0]);
+             }
+	
 	//while((c=fgetc(in))!=EOF)
 	{
 		//putchar(c);
