931245c28595e3c91aa0fc880509576875013865
karippalrohith <77481086+karippalrohith@users.noreply.github.com>
2021-06-21 09:46:27 +0530

    Update BatteryMonitoringSystem_Receiver_Main.c

--- BMSApplication_Receiver/BatteryMonitoringSystem_Receiver_Main.c
+++ BMSApplication_Receiver/BatteryMonitoringSystem_Receiver_Main.c
@@ -41,99 +41,43 @@
  =======                              METHODS                            =======
  =============================================================================*/
 
-#ifndef UNIT_TEST   
-int  readInputFromPipe(char *stringIP)
+
+int main(int argc, char *argv[])
 {
-	int length=0;
-	if(stringIP != NULL)
-	{
-		//scanf("%s",stringIP);
-		//printf("%s\n",stringIP);
-		//printf("%c\n",stringIP[0]);
-    		//length = strlen(stringIP);
-    		printf("%d\n",length);
-		
-    		stringIP[length] = '\n';
-    		stringIP[length + 1] = '\0';
-		
-		return 1;
-	}
-	else
-	{
-		return 0;
-	}
+ FILE *fp;
+ char *filename;
+ char ch;
+
+  // Check if a filename has been specified in the command
+  if (argc < 2)
+   {
+        printf("Missing Filename\n");
+        return(1);
+   }
+   else
+  {
+        filename = argv[1];
+        printf("Filename : %s\n", filename);
+   }
+
+   // Open file in read-only mode
+   fp = fopen(filename,"r");
+
+   // If file opened successfully, then print the contents
+   if ( fp )
+      {
+        printf("File contents:\n");
+        while ( (ch = fgetc(fp)) != EOF )
+           {
+                printf("%c",ch);
+           }
+
+       }
+   else
+      {
+         printf("Failed to open the file\n");
+        }
+
+return(0);
 }
 
-int main(int argc,char *argv[])
-{
-	char stringIP[250];
-	 int p[2], i;
-	
-	int nread = read(p[0], stringIP , 250);
-	   		printf("String is %s\n",stringIP);
-		printf("%c\n",stringIP[0]);
-	printf("%d\n",nread);
-	
-
-	
-	fgets(stringIP, sizeof stringIP, stdin);
-   		printf("%s\n",stringIP);
-		printf("%c\n",stringIP[0]);
-	
-    if (1 == sscanf(stringIP, "%d", &i)) {
-        /* i can be safely used */
-	    printf("%d\n",i);
-	    printf("%c\n",stringIP[0]);
-    }
-	
-	if (stringIP[strlen(stringIP)-1] == '\n') 
-	{
-        	// read full line
-		scanf("%s",stringIP);
-		printf("%s\n",stringIP);
-		printf("%c\n",stringIP[0]);
-    	} else 
-	{
-        	// line was truncated
-    	}
-	
-	int readStatus = readInputFromPipe(stringIP);
-	
-	if(readStatus == 1)
-	{
-		float paramBuffer[2] = { 0.f };
-		
-		  int i = 0, j = 0; //index counter
-
- 		char par[15]; //string for each parameter 
-  		int parNum = 0;  //count of parameters
-
-		// while ((stringIP[i] != '\n'))
-		//{
-		
-					/* Read each parameter */
-					//for (j = 0;stringIP[i] != ',';j++, i++)
-					//{
-					//	par[j] = stringIP[i];
-					//}
-
-					//par[j] = '\0'; //Adding end of string
-		
-					/* Convert string to float number to evaluate further */
-					//paramBuffer[parNum] = atof(par);
-		
-					//printf("Paramter value = %f\n", paramBuffer[parNum]); //for debug purpose
-
-					/* Iterate to next parameter in the buffer*/ 
-					//parNum++; 
-
-					/*Escape Comma separated character*/
-					//i++;
-			//}
-
- 
-		
-	}
-	return 0;
-}
-#endif
