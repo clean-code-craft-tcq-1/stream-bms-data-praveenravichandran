117e953fbe47da48241741a22f03a3cfdeeb0a29
karippalrohith <77481086+karippalrohith@users.noreply.github.com>
2021-06-22 07:39:45 +0530

    Create BatteryMonitoringSystem_Receiver_Test.cpp

---
+++ BMSApplication_Receiver/BatteryMonitoringSystem_Receiver_Test.cpp
@@ -0,0 +1,9 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+
+#include "../testRx/catch.hpp"
+#include "BatteryMonitoringSystem_Receiver_Main.h"
+
+TEST_CASE("Check if all the values received had been send to console") {
+	
+  BMS_Receiver_Main_Function();
+}
