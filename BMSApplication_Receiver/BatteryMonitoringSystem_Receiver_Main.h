589b8201376592d8db989597589cc2b6a9500f38
karippalrohith <77481086+karippalrohith@users.noreply.github.com>
2021-06-22 06:15:52 +0530

    Update BatteryMonitoringSystem_Receiver_Main.h

--- BMSApplication_Receiver/BatteryMonitoringSystem_Receiver_Main.h
+++ BMSApplication_Receiver/BatteryMonitoringSystem_Receiver_Main.h
@@ -3,3 +3,4 @@ float getParameterValue(char * TempString);
 float calculateMinParameterValue(float paramValue, float paramMin);
 float calculateMaxParameterValue(float paramValue, float paramMax);
 void printParameter(char paramName[100],char paramStatus[100],float paramValue);
+float calculateMovingAverage(int count,float param_Value[]);
