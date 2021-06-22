d2a83e1703f7f2c699168f97a836bb22a9d28db5
karippalrohith <77481086+karippalrohith@users.noreply.github.com>
2021-06-22 11:26:04 +0530

    Update BatteryMonitoringSystem_Receiver_Test.cpp

--- BMSApplication_Receiver/BatteryMonitoringSystem_Receiver_Test.cpp
+++ BMSApplication_Receiver/BatteryMonitoringSystem_Receiver_Test.cpp
@@ -3,19 +3,6 @@
 #include "../testRx/catch.hpp"
 #include "BatteryMonitoringSystem_Receiver_Main.h"
 
-TEST_CASE("Check if minimum value for parameter is found or not") {
-	float minValueCurrent = 15;
-	float minValueNew = 0;
-	
-  	minValueNew = BMS_Receiver_calculateMinParameterValue(10,minValueCurrent);
-	REQUIRE(minValueNew == 10);
-	
-	minValueNew = BMS_Receiver_calculateMinParameterValue(23,minValueCurrent);
-	REQUIRE(minValueNew == minValueCurrent);
-	
-	minValueNew = BMS_Receiver_calculateMinParameterValue(15,minValueCurrent);
-	REQUIRE(minValueNew == minValueCurrent);
-}
 
 TEST_CASE("Check if maximum value for parameter is found or not") {
 	float maxValueCurrent = 15;
